d781af2f4aca17988af2322407c9b8306a07cd1f
Johnny Ho <johnnydavidho100106@gmail.com>
2023-05-28 22:39:27 +0100

    Implemented probabilistic prime number generator

--- src/prime_generator.py
+++ src/prime_generator.py
@@ -9,7 +9,7 @@ def is_prime(n: int, k: int = 128):
 
     s = 0
     r = n - 1
-    while r % 2 = 0:
+    while r % 2 == 0:
         s += 1
         r //= 2
 
@@ -21,9 +21,22 @@ def is_prime(n: int, k: int = 128):
 
         for _ in range(s - 1):
             x = pow(x, 2, n)
-            if == n - 1:
+            if x == n - 1:
                 break
         else:
             return False
 
     return True
+
+
+def generate_possible_prime(bits: int = 1024):
+    n = getrandbits(bits)
+    n |= (1 << bits - 1) | 1
+    return n
+
+
+def generate_prime(bits: int = 1024):
+    while not is_prime(n := generate_possible_prime()):
+        pass
+    return n
+    
